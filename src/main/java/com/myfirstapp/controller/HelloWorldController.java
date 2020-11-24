e530665259a3fb8acc081da29301b7617acf53b0
Aditya Verma <aaadarths2357@gmail.com>
2020-11-24 19:29:43 +0530

    Added: user model for the spring app for hello messages

--- src/main/java/com/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/myfirstapp/controller/HelloWorldController.java
@@ -2,10 +2,15 @@ package com.myfirstapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myfirstapp.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -23,6 +28,16 @@ public class HelloWorldController {
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
+	}
+	
+	@PutMapping("/put/{firstName}")
+	public String sayHelloPut(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + "!"; 
 	}
 
 }
